19a36159f5b87b136d5530978f21de7fe1f7f1dc
nupurnigudkar9404 <nupur.nigudkar9404@gmail.com>
2025-06-26 22:54:45 +0530

    add task function added

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -5,8 +5,7 @@ import com.example.todoapp.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +17,15 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    public String getTasks(Model model){
+    public String getTasks(Model model) {
         List<Task> tasks = taskService.getAllTasks();
-        model.addAttribute("tasks",tasks);
-        return "tasks";
+        model.addAttribute("tasks", tasks);
+        return "tasks"; // src/main/resources/templates/tasks.html
+    }
 
+    @PostMapping("/addtask")
+    public String createTask(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/tasks";
     }
 }
